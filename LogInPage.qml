76253b7be12f4570b72fa9bb50fe684eb4bd82f3
tamtam <tam.duong@ascenx.com>
2018-04-13 18:32:58 +0900

    fixed bugs

--- LogInPage.qml
+++ LogInPage.qml
@@ -2,7 +2,6 @@ import QtQuick 2.7
 import QtQuick.Controls 2.0
 import QtQuick.Layouts 1.3
 import QtCharts 2.1
-import QtQuick.VirtualKeyboard 2.1
 import Qt.labs.settings 1.0
 
 Item {
--- flipper.sh
+++ flipper.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+cd /home/pi/FlipperDewPointBinary
  export LD_LIBRARY_PATH=`pwd`/lib
  export QML_IMPORT_PATH=`pwd`/lib/qml
  export QML2_IMPORT_PATH=`pwd`/lib/qml
  export QT_QPA_PLATFORM_PLUGIN_PATH=`pwd`/lib/plugins/platforms
- ./FlipperDewpointMonitoring
+ export QT_PLUGIN_PATH=`pwd`/lib/plugins
+#export QT_IM_MODULE=qtvirtualkeyboard
+/home/pi/FlipperDewPointBinary/FlipperDewpointMonitoring &
